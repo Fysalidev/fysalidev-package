f56d74aa0feb35940432c6766ca79a7c01b59d55
ROBILLIARD Grégory <gregory.robilliard@icloud.com>
2022-12-13 11:55:26 +0100

    Base

---
+++ src/lib/Say/Say.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./styles.css";
+
+function Say({ something }) {
+  return <h1 className="title">{something}</h1>;
+}
+
+export default Say;
---
+++ src/lib/index.js
@@ -0,0 +1,2 @@
+import Say from "./Say/Say"
+export {Say}
